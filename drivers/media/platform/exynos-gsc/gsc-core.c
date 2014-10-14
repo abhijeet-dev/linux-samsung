d26cdbe07b88ad2effb78c854452a8d86a85f3f9
Ulf Hansson <ulf.hansson@linaro.org>
2014-10-14 09:15:36 +0200

    exynos-gsc: Make driver functional without CONFIG_PM_RUNTIME
    The driver depended on CONFIG_PM_RUNTIME to be functional, which isn't
    necessary.
    
    The solution to the above is to enable all runtime PM resourses during
    probe and update the device's runtime PM status to active.
    
    Since driver core invokes pm_request_idle() after ->probe(), unused gsc
    devices will be runtime PM suspended and thus we will still benefit
    from using CONFIG_PM_RUNTIME.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/media/platform/exynos-gsc/gsc-core.c
+++ drivers/media/platform/exynos-gsc/gsc-core.c
@@ -1134,7 +1134,7 @@ static int gsc_probe(struct platform_device *pdev)
 		return PTR_ERR(gsc->clock);
 	}
 
-	ret = clk_prepare(gsc->clock);
+	ret = clk_prepare_enable(gsc->clock);
 	if (ret) {
 		dev_err(&gsc->pdev->dev, "clock prepare failed for clock: %s\n",
 			GSC_CLOCK_GATE_NAME);
@@ -1162,31 +1162,31 @@ static int gsc_probe(struct platform_device *pdev)
 		goto err_v4l2;
 
 	platform_set_drvdata(pdev, gsc);
-	pm_runtime_enable(dev);
-	ret = pm_runtime_get_sync(&pdev->dev);
-	if (ret < 0)
-		goto err_m2m;
+
+	gsc_hw_set_sw_reset(gsc);
+	gsc_wait_reset(gsc);
+	gsc_m2m_resume(gsc);
 
 	/* Initialize continious memory allocator */
 	gsc->alloc_ctx = vb2_dma_contig_init_ctx(dev);
 	if (IS_ERR(gsc->alloc_ctx)) {
 		ret = PTR_ERR(gsc->alloc_ctx);
-		goto err_pm;
+		goto err_m2m;
 	}
 
 	dev_dbg(dev, "gsc-%d registered successfully\n", gsc->id);
 
-	pm_runtime_put(dev);
+	pm_runtime_set_active(dev);
+	pm_runtime_enable(dev);
+
 	return 0;
-err_pm:
-	pm_runtime_put(dev);
 err_m2m:
 	gsc_unregister_m2m_device(gsc);
 err_v4l2:
 	v4l2_device_unregister(&gsc->v4l2_dev);
 err_clk:
 	gsc_iommu_deinit(gsc);
-	clk_unprepare(gsc->clock);
+	clk_disable_unprepare(gsc->clock);
 	return ret;
 }
 
